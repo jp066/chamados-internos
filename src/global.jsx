69855b09fe2f0e880c8a744264e32bdc25bd6a33
jp066 <jpedro030506@gmail.com>
2025-09-15 17:40:20 -0300

    .

--- src/global.jsx
+++ src/global.jsx
@@ -1,7 +1,11 @@
-import { createGlobalStyle } from "styled-components";export default createGlobalStyle`
+import { createGlobalStyle } from "styled-components";
+export default createGlobalStyle`
 *,
 *::after,
-*::before {box-sizing: border-box;  }body {
+*::before {
+  box-sizing: border-box;
+}
+body {
   background: ${({ theme }) => theme.body};
   color: ${({ theme }) => theme.text};
   margin: 0;    
